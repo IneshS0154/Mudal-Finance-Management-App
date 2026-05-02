7c427a99ec56495980d7145e9b6466428d73e7b9
IT24103989 <it24103989@my.sliit.lk>
2026-05-02 22:29:23 +0530

    update base url

--- App/mudal/src/api/client.js
+++ App/mudal/src/api/client.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: 'http://192.168.1.43:5000/api',
+  timeout: 10000,
 });
 
 export default client;
