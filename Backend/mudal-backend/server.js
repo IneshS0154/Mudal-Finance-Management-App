8ea3b698dddb9d4040ac07752725d9fa3622b504
IT24103989 <it24103989@my.sliit.lk>
2026-05-02 22:29:54 +0530

    add route to server js

--- Backend/mudal-backend/server.js
+++ Backend/mudal-backend/server.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
+const recurringRoutes = require('./routes/recurring');
+
+
 dotenv.config();
 connectDB();
 
@@ -10,8 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes go here
-// app.use('/api/auth', require('./routes/auth'));
+// Routes
+app.use('/api/recurring', recurringRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
