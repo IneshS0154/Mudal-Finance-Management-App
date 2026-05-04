63a814f7c0bebaea68e43ce845e5076bf5d13cdc
Maneesha-Prabashwara <mannish1215@gmail.com>
2026-05-04 16:19:16 +0530

    Set up routes for authentication and budgets

--- Backend/mudal-backend/server.js
+++ Backend/mudal-backend/server.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
+// Import Route Files
+const budgetRoutes = require('./routes/budgets');
+const authRoutes = require('./routes/auth');
+
 dotenv.config();
 connectDB();
 
@@ -10,8 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes go here
-// app.use('/api/auth', require('./routes/auth'));
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/budgets', budgetRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
